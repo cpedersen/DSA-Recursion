0bebb6236ee63d4620df44d3799b8ee66ee26b44
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-03 22:39:49 -0500

    Power calc using recursion

---
+++ power-calc.js
@@ -0,0 +1,33 @@
+// What is the input to the program?: (10, 2), (10, -2)
+// What is the output of the program?:
+//      100
+//      exponent should be >= 0
+// What is the input to each recursive call?:
+//      base, exponent - 1
+// What is the output of each recursive call?:
+//      base * powerCalc(base, exponent - 1)
+
+/*function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}*/
+
+const powerCalc = function (base, exponent) {
+  if (exponent == 1) {
+    // Base case
+    return base;
+  } else if (exponent <= 0) {
+    // Error case
+    return "exponent should be >= 0";
+  } else {
+    // Output
+    return base * powerCalc(base, exponent - 1);
+  }
+};
+
+console.log(powerCalc(10, 2));
+console.log(powerCalc(10, -2));
+//console.log(powerCalc(10, 1));
